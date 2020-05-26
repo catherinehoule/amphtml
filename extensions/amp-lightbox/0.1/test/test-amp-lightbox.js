d82a5d432b7a517caa49d17aced268ce1022bc74
catherinehoule <catherinehoule@hotmail.com>
2020-05-26 13:50:13 -0400

    Accessibility focus on lightbox
    evo
    
    improve
    
    cleaning
    
    cleaning
    
    cleaning
    
    add examples and clean
    
    deal with mutate
    
    make close work on ad
    
    fix close-button attribute
    
    clean
    
    things that didnt follow
    
    start tests
    
    test not getting better
    
    Error: Timeout of 2000ms exceeded. For async tests and hooks, ensure done() is called; if returning a Promise, ensure it resolves.
    
    listener only on open
    
    clean

--- extensions/amp-lightbox/0.1/test/test-amp-lightbox.js
+++ extensions/amp-lightbox/0.1/test/test-amp-lightbox.js
@@ -15,13 +15,12 @@
  */
 
 import '../amp-lightbox';
+import * as dom from '../../../../src/dom';
 import {ActionService} from '../../../../src/service/action-impl';
 import {ActionTrust} from '../../../../src/action-constants';
+import {Keys} from '../../../../src/utils/key-codes';
 import {Services} from '../../../../src/services';
-import {
-  createElementWithAttributes,
-  whenUpgradedToCustomElement,
-} from '../../../../src/dom';
+import {whenCalled} from '../../../../testing/test-helper.js';
 
 describes.realWin(
   'amp-lightbox component',
@@ -31,34 +30,54 @@ describes.realWin(
       runtimeOn: true,
     },
   },
-  (env) => {
+  env => {
     let win, doc;
 
-    beforeEach(() => {
+    beforeEach(async () => {
       win = env.win;
       doc = win.document;
     });
+
+    function createLightbox() {
+      const element = dom.createElementWithAttributes(doc, 'amp-lightbox', {
+        'id': 'myLightbox',
+        'layout': 'nodisplay',
+      });
+      const randomButton = dom.createElementWithAttributes(doc, 'button', {
+        'id': 'randomButton',
+      });
+      randomButton.textContent = 'Something to focus on';
+      element.appendChild(randomButton);
+
+      const closeButton = dom.createElementWithAttributes(doc, 'button', {
+        'id': 'closeButton',
+        'on': 'tap:myLightbox.close',
+      });
+      closeButton.textContent = 'X';
+      element.appendChild(closeButton);
+      doc.body.appendChild(element);
+
+      return element;
+    }
+
+    function createButton() {
+      const button = doc.createElement('button');
+      button.textContent = 'Open lightbox';
+      doc.body.appendChild(button);
+      return button;
+    }
 
     it('should allow default actions in email documents', async () => {
       doc.documentElement.setAttribute('amp4email', '');
       const action = new ActionService(env.ampdoc, doc);
       env.sandbox.stub(Services, 'actionServiceForDoc').returns(action);
 
-      const element = createElementWithAttributes(doc, 'amp-lightbox', {
-        'layout': 'nodisplay',
-      });
-      const img = createElementWithAttributes(doc, 'amp-img', {
-        'src': '/examples/img/sample.jpg',
-        'width': '640',
-        'height': '480',
-      });
-      element.appendChild(img);
-      doc.body.appendChild(element);
+      const element = createLightbox();
       env.sandbox.spy(element, 'enqueAction');
       env.sandbox.stub(element, 'getDefaultActionAlias');
-      await whenUpgradedToCustomElement(element);
+      await dom.whenUpgradedToCustomElement(element);
 
-      ['open', 'close'].forEach((method) => {
+      ['open', 'close'].forEach(method => {
         action.execute(
           element,
           method,
@@ -81,6 +100,105 @@ describes.realWin(
           })
         );
       });
+    });
+
+    it('should close on ESC', () => {
+      const lightbox = createLightbox();
+      const impl = lightbox.implementation_;
+      impl.getHistory_ = () => {
+        return {
+          pop: () => {},
+          push: () => Promise.resolve(11),
+        };
+      };
+
+      const sourceElement = doc.createElement('button');
+      sourceElement.textContent = 'Open lightbox';
+      doc.body.appendChild(sourceElement);
+      const nextElement = doc.createElement('button');
+      nextElement.textContent = 'Something to focus on';
+      doc.body.appendChild(nextElement);
+
+      const setupCloseSpy = env.sandbox.spy(impl, 'close');
+
+      impl.open_({caller: sourceElement});
+      impl.closeOnEscape_(new KeyboardEvent('keydown', {key: Keys.ENTER}));
+      impl.closeOnEscape_(new KeyboardEvent('keydown', {key: Keys.ESCAPE}));
+      expect(setupCloseSpy).to.be.calledOnce;
+    });
+
+    // Accessibility
+    // 1. If there is focus in the lightbox, we do not create a top close button or change focus
+    // 2. If there is no focus in the lightbox but has close btn, we focus on it
+    // 3. If there is no focus in the lightbox or close btn, we create a close button and focus on it
+    // 4. If a user "blurs" by focusing on an element that is in the amp-lightbox subtree, it should stay open.
+    // 5a. If a user "blurs" before the amp-lightbox subtree, it should close.
+    // 5b. If a user "blurs" after the amp-lightbox subtree, it should close.
+    // 6. On close, focus should go back to trigger
+    // 7. Ad: we do not create a button and we focus on the `i-amphtml-ad-close-header`
+    //it.only('should not change focus if user has set it already', () => {
+    it.only('should focus on close button if no handmade focus but has close button', () => {
+      const lightbox = createLightbox();
+      const impl = lightbox.implementation_;
+      //const closeButton = createButton();
+      //env.sandbox.stub(impl, 'getExistingCloseButton_').returns(closeButton);
+
+      impl.getHistory_ = () => {
+        return {
+          pop: () => {},
+          push: () => Promise.resolve(11),
+        };
+      };
+
+      const tryFocus = env.sandbox.spy(dom, 'tryFocus');
+      const spy = env.sandbox.spy(impl, 'finalizeOpen_');
+
+      const args = {};
+      const openInvocation = {
+        method: 'open',
+        args,
+        satisfiesTrust: () => true,
+      };
+      impl.executeAction(openInvocation);
+      return whenCalled(spy).then(() => {
+        console.log('4444444444 spy');
+        console.log(tryFocus.getCalls());
+        expect(tryFocus).to.be.calledOnce;
+      });
+    });
+
+    it('should focus on close button if no handmade focus but has close button', () => {
+      const lightbox = createLightbox();
+      const sourceElement = createButton();
+
+      const tryFocus = env.sandbox.spy(dom, 'tryFocus');
+      //const tryOpen = env.sandbox.spy(lightbox, 'open_');
+      const impl = lightbox.implementation_;
+      impl.open_();
+
+      console.log('33333333 spy');
+      console.log(tryFocus.getCalls());
+      //console.log(tryOpen.getCalls());
+      //console.log(env.sandbox.spy.printf('%n / %c fois / %*'));
+      expect(tryFocus).to.be.calledWith('button');
+    });
+
+    it('should return focus to source element after close', () => {
+      const lightbox = createLightbox();
+      const sourceElement = createButton();
+
+      const impl = lightbox.implementation_;
+      tryFocus(sourceElement);
+
+      expect(doc.activeElement).to.equal(sourceElement);
+      impl.open_({caller: sourceElement});
+      expect(doc.activeElement).not.to.equal(sourceElement);
+
+      const tryFocusSpy = env.sandbox.spy(impl, 'tryFocus');
+      impl.close();
+
+      expect(tryFocusSpy).to.be.calledWith(sourceElement);
+      expect(doc.activeElement).to.equal(sourceElement);
     });
   }
 );
